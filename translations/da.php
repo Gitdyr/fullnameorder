c018f055eadf79aff01af20f2203581190d1d456
Kjeld Borch Egevang <kjeld@mail4us.dk>
2024-10-25 09:05:44 +0200

    Added

---
+++ translations/da.php
@@ -0,0 +1,7 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{fullnameorder}prestashop>fullnameorder_ffa816d04a96e93584ef3cbd962b58b6'] = 'Fuldt navn for ordrer';
+$_MODULE['<{fullnameorder}prestashop>fullnameorder_fdb84680529a713f801f524ad991e462'] = 'Vis kundens fulde navn i ordreoversigten.';
+$_MODULE['<{fullnameorder}prestashop>fullnameorder_876f23178c29dc2552c0b48bf23cd9bd'] = 'Er du sikker på, du vi afinstallere?';
